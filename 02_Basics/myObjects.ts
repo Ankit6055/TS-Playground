18933814bce6b75aeaa1221031be012c871c199f
Ankit6055 <ankit.kumarrrrr6055@gmail.com>
2025-09-01 01:21:26 +0530

    Type Aliases

--- 02_Basics/myObjects.ts
+++ 02_Basics/myObjects.ts
@@ -8,7 +8,7 @@ function createUser({name: string, isPaid: boolean}) {}
 
 let newUser = {name: "ankit", isPaid: false, email: "a@a.com"}
 
-createUser(newUser) // this is a odd behaviour
+createUser(newUser) // this is a odd behaviour, but it will work smoothly
 // createUser({name: "ankit", isPaid: false, email: "a@a.com"}) -> This will not work coz of email
 
 // function createCourse(): {} {
@@ -18,3 +18,17 @@ createUser(newUser) // this is a odd behaviour
 function createCourse(): {name: string, price: number} {
     return {name: "reactjs", price: 399}
 }
+
+// Type Aliases
+
+type User = {
+    name: string;
+    email: string;
+    isActive: boolean
+}
+
+function create_User(user: User): User {
+    return {name: "ankit", email: "a@a.com", isActive: true}
+}
+
+create_User({name: "ankit", email: "a@a.com", isActive: true})
