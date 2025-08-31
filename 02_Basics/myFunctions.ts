ba72ef5b4dd97a7a101c54b202ccfb949169c747
Ankit6055 <ankit.kumarrrrr6055@gmail.com>
2025-09-01 01:02:48 +0530

    Function Advance

--- 02_Basics/myFunctions.ts
+++ 02_Basics/myFunctions.ts
@@ -1,6 +1,6 @@
 function addTwo(num: number) {
-//   return num + 2;
-  return "hello"
+  return num + 2;
+  //   return "hello" ❌
 }
 
 function getUpper(val: string) {
@@ -16,5 +16,34 @@ let myValue: number = addTwo(5);
 getUpper("4");
 signUpUser("Ankit", "a@a.com", true);
 loginUser("A", "a@a.com");
+
+// function getValue(myVal: number): boolean {
+//   if (myVal > 5) {
+//     return true;
+//   }
+//   return "200 OK"; -> This will throw an error
+// }
+
+const getHello = (s: string): string => {
+  return "";
+};
+
+const heros = ["thor", "spiderman", "ironman"];
+// const heros = [1, 2, 3]
+
+heros.map((hero): string => {
+  return `hero is ${hero}`;
+});
+
+function consoleError(errmsg: string): void {
+  // void means nothing
+  console.log(errmsg);
+//   return; -> We can write this also, it will be valid
+}
+
+function handleError(errmsg: string): never {
+  // the func never returns a value
+  throw new Error(errmsg);
+}
 
 export {};
