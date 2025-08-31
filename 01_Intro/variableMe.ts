3856dfb5b9f69d7fb274b752e2c37859b5f887b4
Ankit6055 <ankit.kumarrrrr6055@gmail.com>
2025-09-01 00:30:37 +0530

    Boolean and number done

--- 01_Intro/variableMe.ts
+++ 01_Intro/variableMe.ts
@@ -1,10 +1,21 @@
 let greetings: string = "Hello Ankit";
 let mynum = 6;
 
-
 // greetings = 6 -> Not allowed, this is the safety which typescript gives
 // mynum.toUpperCase() -> Not allowed, coz to ts, we avoided a error
 
 console.log(greetings);
 
-export {}
+// number
+
+// let userId: number = 334466.9898; type can be avoided, ts will automatically detect it's type is number
+let userId = 334466;
+
+userId.toFixed()
+
+// boolean
+let isLoggedIn: boolean = false;
+
+
+
+export {};
