848ff8942ce74e8c329e7f2de1207a6d15855513
Ankit6055 <ankit.kumarrrrr6055@gmail.com>
2025-09-01 00:10:16 +0530

    Intro file executed

---
+++ 01_Intro/intro.js
@@ -0,0 +1,3 @@
+var user = { name: "Ankit", age: 22 };
+console.log("Ankit");
+console.log(user.name);
---
+++ 01_Intro/intro.ts
@@ -0,0 +1,6 @@
+let user = {name: "Ankit", age: 22}
+
+console.log("Ankit");
+console.log(user.name)
+
+// tsc intro.ts -> intro.js (we run/execute this file)
